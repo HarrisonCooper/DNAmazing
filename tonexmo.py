fc2de5043cfa80816848999215ed333982488418
[Gemma-Rate] <[thebrickmaster@theratefamily.co.uk]>
2017-04-29 16:14:08 +0100

    Initial file for creation of nexmo call.

---
+++ tonexmo.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 29 11:53:46 2017
+
+@author: Gemma
+"""
+
+import nexmo
+
+class Application(object):
+    #Creates applications. 
+    pass
+
+class Event(object):
+    #Creates events.
+    pass
+
+
+class Caller(object):
+    
+    def buynumber():
+        #Purchases number.
+        pass
+
+    def associate():
+        #Associates number with application. 
+        pass
+
+
